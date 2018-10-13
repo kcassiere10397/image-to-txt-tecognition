f09da104633d83bbf222827c7b3891da8ec3d97d
Paul Matthew Barrameda <luisanduis729@gmail.com>
2018-10-12 21:48:19 -0700

    Setup files

---
+++ src/main/index.js
@@ -0,0 +1,12 @@
+// https://github.com/electron-userland/electron-webpack-quick-start/blob/master/src/main/index.js
+
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import url from 'url';
+
+let mainWindow;
+
+function createMainWindow() {
+  const window = new BrowserWindow();
+
+}
